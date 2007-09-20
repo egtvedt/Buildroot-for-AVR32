4641bab37554fc73cbf1ffaa6060c7656b578f50
Hans-Christian Egtvedt <hcegtvedt@atmel.com>
2007-09-20 08:10:02 +0200

    libid3tag: add dependance on zlib to Makefile and cleanup Makefile
    Signed-off-by: Hans-Christian Egtvedt <hcegtvedt@atmel.com>

--- package/libid3tag/libid3tag.mk
+++ package/libid3tag/libid3tag.mk
@@ -3,10 +3,11 @@
 # libid3tag
 #
 #############################################################
-LIBID3TAG_VER:=0.15.1b
-LIBID3TAG_SOURCE:=libid3tag-$(LIBID3TAG_VER).tar.gz
+
+LIBID3TAG_VERSION:=0.15.1b
+LIBID3TAG_SOURCE:=libid3tag-$(LIBID3TAG_VERSION).tar.gz
 LIBID3TAG_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/mad
-LIBID3TAG_DIR:=$(BUILD_DIR)/libid3tag-$(LIBID3TAG_VER)
+LIBID3TAG_DIR:=$(BUILD_DIR)/libid3tag-$(LIBID3TAG_VERSION)
 LIBID3TAG_CAT:=$(ZCAT)
 LIBID3TAG_BIN:=libid3tag.so.0.3.0
 LIBID3TAG_TARGET_BIN:=usr/lib/$(LIBID3TAG_BIN)
@@ -16,9 +17,9 @@ $(DL_DIR)/$(LIBID3TAG_SOURCE):
 
 $(LIBID3TAG_DIR)/.unpacked: $(DL_DIR)/$(LIBID3TAG_SOURCE)
 	$(LIBID3TAG_CAT) $(DL_DIR)/$(LIBID3TAG_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(LIBID3TAG_DIR) package/libid3tag/ libid3tag-$(LIBID3TAG_VER)\*.patch\*
+	toolchain/patch-kernel.sh $(LIBID3TAG_DIR) package/libid3tag/ libid3tag-$(LIBID3TAG_VERSION)\*.patch\*
 	$(CONFIG_UPDATE) $(LIBID3TAG_DIR)
-	@touch $@
+	touch $@
 
 $(LIBID3TAG_DIR)/.configured: $(LIBID3TAG_DIR)/.unpacked
 	(cd $(LIBID3TAG_DIR); rm -rf config.cache; \
@@ -31,8 +32,8 @@ $(LIBID3TAG_DIR)/.configured: $(LIBID3TAG_DIR)/.unpacked
 		--prefix=/usr \
 		--sysconfdir=/etc \
 		$(DISABLE_NLS) \
-	);
-	@touch $@
+	)
+	touch $@
 
 $(LIBID3TAG_DIR)/.libs/$(LIBID3TAG_BIN): $(LIBID3TAG_DIR)/.configured
 	$(MAKE) -C $(LIBID3TAG_DIR)
@@ -42,19 +43,22 @@ $(STAGING_DIR)/$(LIBID3TAG_TARGET_BIN): $(LIBID3TAG_DIR)/.libs/$(LIBID3TAG_BIN)
 	$(SED) "s,^libdir=.*,libdir=\'$(STAGING_DIR)/usr/lib\',g" $(STAGING_DIR)/usr/lib/libid3tag.la
 
 $(TARGET_DIR)/$(LIBID3TAG_TARGET_BIN): $(STAGING_DIR)/$(LIBID3TAG_TARGET_BIN)
-	cp -dpf  $(STAGING_DIR)/usr/lib/libid3tag.so*  $(TARGET_DIR)/usr/lib/
-	-$(STRIP) --strip-unneeded $(TARGET_DIR)/usr/lib/libid3tag.so*
+	cp -dpf $(STAGING_DIR)/usr/lib/libid3tag.so* $(TARGET_DIR)/usr/lib/
+	-$(STRIP) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libid3tag.so*
 
-libid3tag: uclibc libmad $(TARGET_DIR)/$(LIBID3TAG_TARGET_BIN)
+libid3tag: uclibc zlib libmad $(TARGET_DIR)/$(LIBID3TAG_TARGET_BIN)
+
+libid3tag-source: $(DL_DIR)/$(LIBID3TAG_SOURCE)
 
 libid3tag-clean:
+	@if [ -d $(LIBID3TAG_DIR)/Makefile ]; then \
+		$(MAKE) -C $(LIBID3TAG_DIR) clean; \
+	fi
+	rm -f $(STAGING_DIR)/$(LIBID3TAG_TARGET_BIN)
 	rm -f $(TARGET_DIR)/$(LIBID3TAG_TARGET_BIN)
-	-$(MAKE) -C $(LIBID3TAG_DIR) clean
 
 libid3tag-dirclean:
 	rm -rf $(LIBID3TAG_DIR)
-
-libid3tag-source: $(DL_DIR)/$(LIBID3TAG_SOURCE)
 #############################################################
 #
 # Toplevel Makefile options
