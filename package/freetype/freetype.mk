c30ed4e6a5a4b5cdfc5382cad559420d96a698a1
Hans-Christian Egtvedt <hcegtvedt@atmel.com>
2007-11-16 13:58:34 +0100

    freetype: bump version to 2.3.5 and clean up Makefile
    This patch cleans up the Makefile by changing all the touch some file to touch
    $@. It also installs the library to the correct folder on target, /usr/lib.
    
    The different arguments to ./configure are also simplified, doing as much as
    possible with the --prefix parameter.
    
    Signed-off-by: Hans-Christian Egtvedt <hcegtvedt@atmel.com>

--- package/freetype/freetype.mk
+++ package/freetype/freetype.mk
@@ -3,7 +3,7 @@
 # freetype
 #
 #############################################################
-FREETYPE_VERSION:=2.2.1
+FREETYPE_VERSION:=2.3.5
 FREETYPE_SOURCE:=freetype-$(FREETYPE_VERSION).tar.bz2
 FREETYPE_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/freetype
 FREETYPE_CAT:=$(BZCAT)
@@ -18,7 +18,8 @@ freetype-source: $(DL_DIR)/$(FREETYPE_SOURCE)
 
 $(FREETYPE_DIR)/.unpacked: $(DL_DIR)/$(FREETYPE_SOURCE)
 	$(FREETYPE_CAT) $(DL_DIR)/$(FREETYPE_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	touch $(FREETYPE_DIR)/.unpacked
+	$(CONFIG_UPDATE) $(FREETYPE_DIR)/builds/unix
+	touch $@
 
 # freetype for the target
 $(FREETYPE_DIR)/.configured: $(FREETYPE_DIR)/.unpacked
@@ -30,56 +31,49 @@ $(FREETYPE_DIR)/.configured: $(FREETYPE_DIR)/.unpacked
 		--host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
 		--prefix=/usr \
-		--exec-prefix=/usr \
-		--bindir=/usr/bin \
-		--sbindir=/usr/sbin \
-		--libdir=/lib \
 		--libexecdir=/usr/lib \
 		--sysconfdir=/etc \
-		--datadir=/usr/share \
 		--localstatedir=/var \
-		--includedir=/include \
-		--mandir=/usr/man \
-		--infodir=/usr/info \
-	);
-	touch $(FREETYPE_DIR)/.configured
+	)
+	touch $@
 
 $(FREETYPE_DIR)/.compiled: $(FREETYPE_DIR)/.configured
 	$(MAKE) CCexe="$(HOSTCC)" -C $(FREETYPE_DIR)
-	touch $(FREETYPE_DIR)/.compiled
+	touch $@
 
-$(STAGING_DIR)/lib/libfreetype.so: $(FREETYPE_DIR)/.compiled
+$(STAGING_DIR)/usr/lib/libfreetype.so: $(FREETYPE_DIR)/.compiled
 	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(FREETYPE_DIR) install
-	$(SED) "s,^libdir=.*,libdir=\'$(STAGING_DIR)/lib\',g" $(STAGING_DIR)/lib/libfreetype.la
+	$(SED) "s,^libdir=.*,libdir=\'$(STAGING_DIR)/lib\',g" $(STAGING_DIR)/usr/lib/libfreetype.la
 	$(SED) "s,^prefix=.*,prefix=\'$(STAGING_DIR)\',g" \
 		-e "s,^exec_prefix=.*,exec_prefix=\'$(STAGING_DIR)/usr\',g" \
 		-e "s,^includedir=.*,includedir=\'$(STAGING_DIR)/include\',g" \
 		-e "s,^libdir=.*,libdir=\'$(STAGING_DIR)/lib\',g" \
 		$(STAGING_DIR)/usr/bin/freetype-config
-	touch -c $(STAGING_DIR)/lib/libfreetype.so
+	touch -c $@
 
-$(TARGET_DIR)/lib/libfreetype.so: $(STAGING_DIR)/lib/libfreetype.so
-	cp -dpf $(STAGING_DIR)/lib/libfreetype.so* $(TARGET_DIR)/lib/
-	-$(STRIP) --strip-unneeded $(TARGET_DIR)/lib/libfreetype.so
+$(TARGET_DIR)/usr/lib/libfreetype.so: $(STAGING_DIR)/usr/lib/libfreetype.so
+	cp -dpf $(STAGING_DIR)/usr/lib/libfreetype.so* $(TARGET_DIR)/usr/lib/
+	-$(STRIP) --strip-unneeded $(TARGET_DIR)/usr/lib/libfreetype.so
 
 # freetype for the host, needed for build-tools of fontconfig
 
 # great, it can't be built out of tree reliably
 $(FREETYPE_DIR1)/.unpacked: $(DL_DIR)/$(FREETYPE_SOURCE)
 	$(FREETYPE_CAT) $(DL_DIR)/$(FREETYPE_SOURCE) | tar -C $(TOOL_BUILD_DIR) $(TAR_OPTIONS) -
-	touch $(FREETYPE_DIR1)/.unpacked
+	$(CONFIG_UPDATE) $(FREETYPE_DIR1)/builds/unix
+	touch $@
 
 $(FREETYPE_DIR1)/.configured: $(FREETYPE_DIR1)/.unpacked
 	(cd $(FREETYPE_DIR1); \
 	./configure \
 		CC="$(HOSTCC)" \
 		--prefix="$(FREETYPE_HOST_DIR)" \
-	);
-	touch $(FREETYPE_DIR1)/.configured
+	)
+	touch $@
 
 $(FREETYPE_DIR1)/.compiled: $(FREETYPE_DIR1)/.configured
 	$(MAKE) CCexe="$(HOSTCC)" -C $(FREETYPE_DIR1)
-	touch $(FREETYPE_DIR1)/.compiled
+	touch $@
 
 $(FREETYPE_HOST_DIR)/lib/libfreetype.so: $(FREETYPE_DIR1)/.configured
 	$(MAKE) -C $(FREETYPE_DIR1) install
@@ -87,7 +81,7 @@ $(FREETYPE_HOST_DIR)/lib/libfreetype.so: $(FREETYPE_DIR1)/.configured
 
 host-freetype: $(FREETYPE_HOST_DIR)/lib/libfreetype.so
 
-freetype: uclibc pkgconfig $(TARGET_DIR)/lib/libfreetype.so
+freetype: uclibc pkgconfig $(TARGET_DIR)/usr/lib/libfreetype.so
 
 freetype-clean:
 	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(FREETYPE_DIR) uninstall
