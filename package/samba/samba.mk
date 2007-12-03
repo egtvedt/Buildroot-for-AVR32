16ca619d149c767c23085404c88d640e97ce19e4
Hans-Christian Egtvedt <hcegtvedt@atmel.com>
2007-12-03 12:53:44 +0100

    samba: bump version to 3.0.27a and add a more stable download URL
    Signed-off-by: Hans-Christian Egtvedt <hcegtvedt@atmel.com>

--- package/samba/samba.mk
+++ package/samba/samba.mk
@@ -3,9 +3,9 @@
 # samba
 #
 #############################################################
-SAMBA_VERSION:=3.0.26a
+SAMBA_VERSION:=3.0.27a
 SAMBA_SOURCE:=samba-$(SAMBA_VERSION).tar.gz
-SAMBA_SITE:=ftp://us4.samba.org/pub/samba/stable
+SAMBA_SITE:=http://samba.org/samba/ftp/stable
 SAMBA_DIR:=$(BUILD_DIR)/samba-$(SAMBA_VERSION)/source
 SAMBA_CAT:=$(ZCAT)
 SAMBA_BINARY:=bin/smbd
