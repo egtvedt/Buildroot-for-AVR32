d7ab303ab90e46a79a1d799c1c27e06113fa9900
Haavard Skinnemoen <hskinnemoen@atmel.com>
2007-12-07 14:23:36 +0100

    Bump LTP testsuite to version 20071130 and add two fixes
    The existing LTP snapshot doesn't work on Linux 2.6.23. Upgrade to a
    newer version and add two fixes that turned out to be needed: uClibc
    doesn't have an implementation of remap_file_pages(), and one of the
    tests included some incredibly stupid code that gcc turned into an
    infinite loop.
    
    Signed-off-by: Haavard Skinnemoen <hskinnemoen@atmel.com>

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -3,7 +3,7 @@
 # ltp-testsuite
 #
 #############################################################
-LTP_TESTSUITE_VERSION:=20070228
+LTP_TESTSUITE_VERSION:=20071130
 LTP_TESTSUITE_SOURCE:=ltp-full-$(LTP_TESTSUITE_VERSION).tgz
 LTP_TESTSUITE_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/ltp
 LTP_TESTSUITE_CAT:=$(ZCAT)
