a3877589225fdc691b37249cb446cf6804fc2dd6
Hans-Christian Egtvedt <hcegtvedt@atmel.com>
2008-03-04 10:03:42 +0100

    Import upstream snapshot buildroot-20080304.tar.bz2
    Summary: Import upstream snapshot buildroot-20080304.tar.bz2
    Keywords:
    
    Imported buildroot
    into Git repository
    
    Signed-off-by: Hans-Christian Egtvedt <hcegtvedt@atmel.com>

--- package/dnsmasq/dnsmasq.mk
+++ package/dnsmasq/dnsmasq.mk
@@ -5,22 +5,26 @@
 #############################################################
 
 DNSMASQ_SITE:=http://thekelleys.org.uk/dnsmasq
-ifeq ($(filter $(TARGETS),dnsmasq1),)
-DNSMASQ_UPVER:=2.40
-DNSMASQ_VERSION:=dnsmasq2
-else
-DNSMASQ_UPVER:=1.18
-DNSMASQ_VERSION:=dnsmasq1
-endif
+DNSMASQ_UPVER:=2.41
 DNSMASQ_SOURCE:=dnsmasq-$(DNSMASQ_UPVER).tar.gz
 DNSMASQ_DIR:=$(BUILD_DIR)/dnsmasq-$(DNSMASQ_UPVER)
 DNSMASQ_BINARY:=dnsmasq
 DNSMASQ_TARGET_BINARY:=usr/sbin/dnsmasq
 
-ifneq ($(BR2_INET_IPV6),y)
-DNSMASQ_COPTS:=-DNO_IPV6
-else
 DNSMASQ_COPTS:=
+
+ifneq ($(BR2_INET_IPV6),y)
+DNSMASQ_COPTS+=-DNO_IPV6
+endif
+
+ifneq ($(BR2_PACKAGE_DNSMASQ_TFTP),y)
+DNSMASQ_COPTS+=-DNO_TFTP
+endif
+
+ifeq ($(strip $(BR2_PACKAGE_DBUS)),y)
+DNSMASQ_DBUS:=$(TARGET_DIR)/usr/bin/dbus-daemon
+else
+DNSMASQ_DBUS:=
 endif
 
 $(DL_DIR)/$(DNSMASQ_SOURCE):
@@ -28,27 +32,31 @@ $(DL_DIR)/$(DNSMASQ_SOURCE):
 
 $(DNSMASQ_DIR)/.source: $(DL_DIR)/$(DNSMASQ_SOURCE)
 	$(ZCAT) $(DL_DIR)/$(DNSMASQ_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(DNSMASQ_DIR) package/dnsmasq/ \
-		$(DNSMASQ_VERSION)\*.patch
+	toolchain/patch-kernel.sh $(DNSMASQ_DIR) package/dnsmasq/ \*.patch
 	touch $@
 
-$(DNSMASQ_DIR)/src/$(DNSMASQ_BINARY): $(DNSMASQ_DIR)/.source
+$(DNSMASQ_DIR)/src/$(DNSMASQ_BINARY): $(DNSMASQ_DIR)/.source $(DNSMASQ_DBUS)
+ifeq ($(strip $(BR2_PACKAGE_DBUS)),y)
+	$(SED) 's^.*#define HAVE_DBUS.*^#define HAVE_DBUS^' \
+		$(DNSMASQ_DIR)/src/config.h
+else
+	$(SED) 's^.*#define HAVE_DBUS.*^/* #define HAVE_DBUS */^' \
+		$(DNSMASQ_DIR)/src/config.h
+endif
 	$(MAKE) CC=$(TARGET_CC) CFLAGS="$(TARGET_CFLAGS)" \
-		COPTS=$(DNSMASQ_COPTS) PREFIX=/usr -C $(DNSMASQ_DIR)
+		COPTS='$(DNSMASQ_COPTS)' PREFIX=/usr -C $(DNSMASQ_DIR)
 
 $(TARGET_DIR)/$(DNSMASQ_TARGET_BINARY): $(DNSMASQ_DIR)/src/$(DNSMASQ_BINARY)
 	$(MAKE) DESTDIR=$(TARGET_DIR) PREFIX=/usr -C $(DNSMASQ_DIR) install
 	$(STRIPCMD) $(TARGET_DIR)/$(DNSMASQ_TARGET_BINARY)
 	mkdir -p $(TARGET_DIR)/var/lib/misc
-	# Isn't this vulverable to symlink attacks?
+	# Isn't this vulnerable to symlink attacks?
 	ln -sf /tmp/dnsmasq.leases $(TARGET_DIR)/var/lib/misc/dnsmasq.leases
 ifneq ($(BR2_HAVE_MANPAGES),y)
 	rm -rf $(TARGET_DIR)/usr/share/man
 endif
 
 dnsmasq: uclibc $(TARGET_DIR)/$(DNSMASQ_TARGET_BINARY)
-
-dnsmasq1: uclibc $(TARGET_DIR)/$(DNSMASQ_TARGET_BINARY)
 
 dnsmasq-source: $(DL_DIR)/$(DNSMASQ_SOURCE)
 
