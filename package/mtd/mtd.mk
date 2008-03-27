335c348790c8e907ddb5fd340bd7166fb3f9df98
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2008-03-27 10:22:00 +0100

    MTD: make sure mtd-utils Makefile is loaded if target uses JFFS2 file system
    This patch will make sure mtd-utils Makefile is loaded if the user wants to
    generate a JFFS2 file system, but does not want to include any mtd-utils on the
    target file system.
    
    Signed-off-by: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>

--- package/mtd/mtd.mk
+++ package/mtd/mtd.mk
@@ -1,12 +1,20 @@
 ifeq ($(strip $(BR2_PACKAGE_MTD_UTILS)),y)
-include package/mtd/mtd-utils/mtd.mk
-endif
-ifeq ($(strip $(BR2_PACKAGE_MTD_20061007)),y)
-include package/mtd/20061007/mtd.mk
-endif
-ifeq ($(strip $(BR2_PACKAGE_MTD_20050122)),y)
-include package/mtd/20050122/mtd.mk
-endif
-ifeq ($(strip $(BR2_PACKAGE_MTD_SNAPSHOT)),y)
-include package/mtd/20050122/mtd.mk
+  include package/mtd/mtd-utils/mtd.mk
+else
+  ifeq ($(strip $(BR2_PACKAGE_MTD_20061007)),y)
+    include package/mtd/20061007/mtd.mk
+  else
+    ifeq ($(strip $(BR2_PACKAGE_MTD_20050122)),y)
+      include package/mtd/20050122/mtd.mk
+    else
+      ifeq ($(strip $(BR2_PACKAGE_MTD_SNAPSHOT)),y)
+        include package/mtd/20050122/mtd.mk
+      else
+        # If we are generating a JFFS2 root file system, we need mtd-utils.
+        ifeq ($(strip $(BR2_TARGET_ROOTFS_JFFS2)),y)
+          include package/mtd/mtd-utils/mtd.mk
+        endif
+      endif
+    endif
+  endif
 endif
