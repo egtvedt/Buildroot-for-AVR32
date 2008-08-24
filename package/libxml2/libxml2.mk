cc4f9a9ad2645dd490de9009961087e9951c976a
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2008-08-24 09:42:20 +0200

    Import upstream snapshot buildroot-20080824.tar.bz2
    Summary: Import upstream snapshot buildroot-20080824.tar.bz2
    Keywords:
    
    Imported buildroot
    into Git repository
    
    Signed-off-by: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>

--- package/libxml2/libxml2.mk
+++ package/libxml2/libxml2.mk
@@ -10,19 +10,13 @@ LIBXML2_SITE = ftp://xmlsoft.org/libxml2
 LIBXML2_AUTORECONF = NO
 LIBXML2_INSTALL_STAGING = YES
 LIBXML2_INSTALL_TARGET = YES
+LIBXML2_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) install-strip
 
 ifneq ($(BR2_LARGEFILE),y)
 LIBXML2_CONF_ENV = CC="$(TARGET_CC) $(TARGET_CFLAGS) -DNO_LARGEFILE_SOURCE"
 endif
 
-LIBXML2_CONF_OPT = --target=$(GNU_TARGET_NAME) --host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) --prefix=/usr \
-		--exec-prefix=/usr --bindir=/usr/bin \
-		--sbindir=/usr/sbin --libexecdir=/usr/lib \
-		--sysconfdir=/etc --datadir=/usr/share \
-		--localstatedir=/var --mandir=/usr/man \
-		--infodir=/usr/info --includedir=/usr/include \
-		--with-gnu-ld --enable-shared \
+LIBXML2_CONF_OPT = --with-gnu-ld --enable-shared \
 		--enable-static --enable-ipv6=no \
 		--without-debugging --without-python \
 		--without-threads $(DISABLE_NLS)
@@ -32,5 +26,7 @@ LIBXML2_DEPENDENCIES = uclibc
 $(eval $(call AUTOTARGETS,package,libxml2))
 
 $(LIBXML2_HOOK_POST_INSTALL):
-	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libxml2*so*
+	rm -rf $(TARGET_DIR)/usr/share/aclocal \
+	       $(TARGET_DIR)/usr/share/doc/libxml2-$(LIBXML2_VERSION) \
+	       $(TARGET_DIR)/usr/share/gtk-doc
 	touch $@
