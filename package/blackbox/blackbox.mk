074544485fe716345a93dcd9f31de2b5fb10b8f6
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2008-08-26 09:42:23 +0200

    Import upstream snapshot buildroot-20080827.tar.bz2
    Summary: Import upstream snapshot buildroot-20080827.tar.bz2
    Keywords:
    
    Imported buildroot
    into Git repository
    
    Signed-off-by: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>

--- package/blackbox/blackbox.mk
+++ package/blackbox/blackbox.mk
@@ -4,20 +4,17 @@
 #
 #############################################################
 
-BLACKBOX_VERSION = 0.70.1
-BLACKBOX_SOURCE = blackbox-$(BLACKBOX_VERSION).tar.bz2
-BLACKBOX_SITE = http://superb-west.dl.sourceforge.net/sourceforge/blackboxwm/
-BLACKBOX_AUTORECONF = NO
-BLACKBOX_INSTALL_STAGING = NO
-BLACKBOX_INSTALL_TARGET = YES
-BLACKBOX_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
+BLACKBOX_VERSION:=0.70.1
+BLACKBOX_SOURCE:=blackbox-$(BLACKBOX_VERSION).tar.bz2
+BLACKBOX_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/blackboxwm/
+BLACKBOX_AUTORECONF:=NO
+BLACKBOX_INSTALL_STAGING:=NO
+BLACKBOX_INSTALL_TARGET:=YES
+BLACKBOX_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) install-strip
 
-BLACKBOX_CONF_OPT =	--target=$(GNU_TARGET_NAME) --host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) --prefix=/usr \
-		--sysconfdir=/etc --x-includes=$(STAGING_DIR)/usr/include/X11 \
+BLACKBOX_CONF_OPT:=--x-includes=$(STAGING_DIR)/usr/include/X11 \
 		--x-libraries=$(STAGING_DIR)/usr/lib
 
-
-BLACKBOX_DEPENDENCIES = uclibc $(XSERVER)
+BLACKBOX_DEPENDENCIES:=uclibc $(XSERVER)
 
 $(eval $(call AUTOTARGETS,package,blackbox))
--- package/pixman/pixman.mk
+++ package/pixman/pixman.mk
@@ -3,15 +3,12 @@
 # pixman
 #
 ################################################################################
-
 PIXMAN_VERSION = 0.10.0
 PIXMAN_SOURCE = pixman-$(PIXMAN_VERSION).tar.gz
 PIXMAN_SITE = http://cairographics.org/releases/
 PIXMAN_AUTORECONF = NO
 PIXMAN_INSTALL_STAGING = YES
-PIXMAN_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) install install-data
-PIXMAN_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install install-data
+PIXMAN_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) install
+PIXMAN_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install-strip
 
-PIXMAN_DEPENDENCIES = xlib_libX11 xlib_libXext xproto_dmxproto
-
-$(eval $(call AUTOTARGETS,package/x11r7,pixman))
+$(eval $(call AUTOTARGETS,package,pixman))
