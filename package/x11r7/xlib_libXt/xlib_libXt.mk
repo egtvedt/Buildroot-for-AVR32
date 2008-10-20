c89d3ae08a2fce30a54905040518a5c2c39f6b8a
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2008-10-20 09:42:27 +0200

    Import upstream snapshot buildroot-20081020.tar.bz2
    Summary: Import upstream snapshot buildroot-20081020.tar.bz2
    Keywords:
    
    Imported buildroot
    into Git repository
    
    Signed-off-by: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>

--- package/x11r7/xlib_libXt/xlib_libXt.mk
+++ package/x11r7/xlib_libXt/xlib_libXt.mk
@@ -9,12 +9,10 @@ XLIB_LIBXT_SOURCE = libXt-$(XLIB_LIBXT_VERSION).tar.bz2
 XLIB_LIBXT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXT_AUTORECONF = NO
 XLIB_LIBXT_INSTALL_STAGING = YES
+XLIB_LIBXT_INSTALL_TARGET = YES
+XLIB_LIBXT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install-strip
 XLIB_LIBXT_DEPENDENCIES = xlib_libSM xlib_libX11 xproto_kbproto xproto_xproto xcb-proto libxcb
 XLIB_LIBXT_CONF_ENV = CC_FOR_BUILD="/usr/bin/gcc -I$(STAGING_DIR)/usr/include"
-XLIB_LIBXT_CONF_OPT = --disable-malloc0returnsnull --enable-shared --disable-static
+XLIB_LIBXT_CONF_OPT = --disable-malloc0returnsnull --enable-shared --disable-static --disable-install-makestrs
 
 $(eval $(call AUTOTARGETS,package/x11r7,xlib_libXt))
-
-# the host makestrs shouldn't get installed in target
-$(XLIB_LIBXT_HOOK_POST_INSTALL):
-	rm -f $(TARGET_DIR)/usr/bin/makestrs
