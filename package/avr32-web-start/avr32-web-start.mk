56faf7903cafe64721756fed62e180927c3f8aa1
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2011-01-14 13:09:54 +0100

    avr32-web-start: disable Samba swat part unconditionally
    This patch will disable the Samba swat part unconditionally since Buildroot
    does not start that service even if you have selected to install the
    application.
    
    Signed-off-by: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>

--- package/avr32-web-start/avr32-web-start.mk
+++ package/avr32-web-start/avr32-web-start.mk
@@ -26,10 +26,10 @@ else
 	ln -sf ../cgi-bin/gpio.sh $(TARGET_DIR)/www/src/gpio.sh
 	ln -sf ../cgi-bin/gpio-trigger.sh $(TARGET_DIR)/www/src/gpio-trigger.sh
 endif
-ifneq ($(strip $(BR2_PACKAGE_SAMBA_SWAT)),y)
+# Swat is not enabled as a service out of the box when installed, reinstate
+# enabling/disabling Swat once it is actually running when installed to target.
 	$(SED) 's/\%SWAT_ENABLED_BEGIN\% -->/SWAT_DISABLED/' $(TARGET_DIR)/www/$(AVR32_WEB_START_TARGET)
 	$(SED) 's/<!-- \%SWAT_ENABLED_END\%/SWAT_DISABLED/' $(TARGET_DIR)/www/$(AVR32_WEB_START_TARGET)
-endif
 	ln -sf $(AVR32_WEB_START_TARGET) $(TARGET_DIR)/www/index.asp
 	touch $@
 
